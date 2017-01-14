5a05f7bf437de739f677291add076a30c938225c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-01-14 18:39:02 +0100

    Fix sol1 to Q4 to support uppercase and lowercase.

--- src/Chapter1/Q4IsPermutationOfPalindrome.hs
+++ src/Chapter1/Q4IsPermutationOfPalindrome.hs
@@ -1,12 +1,12 @@
-
 module Q4IsPermutationOfPalindrome where
 
 import Data.List
+import Data.Char (toLower)
 
 isPalindrome :: String -> Bool
 isPalindrome x = x == reverse x
 
 isPermutationOfPalindrome :: String -> Bool
-isPermutationOfPalindrome x = (length $ filter isPalindrome (permutations (filter (/= ' ') x) :: [String])) > 0
-
+isPermutationOfPalindrome x = (length $ filter isPalindrome (permutations cleanString :: [String])) > 0
+  where cleanString = (filter (/= ' ') $ map toLower x) :: String
 
