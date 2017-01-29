c24b14fa6630a7de20fcd2852457d559ba7f00a6
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-01-29 17:02:13 +0100

    Fix bug in solution #9 and adding a point free solution.

--- src/Chapter1/Q9IsRotation.hs
+++ src/Chapter1/Q9IsRotation.hs
@@ -2,7 +2,11 @@
 module Q9IsRotation where
 
 import Data.List
+import Control.Monad
 
 isRotation :: [Char] -> [Char] -> Bool
-isRotation s1 s2 = s2 `isInfixOf` (s1 ++ s1)
+isRotation s1 s2 = length s1 == length s2 && s2 `isInfixOf` (s1 ++ s1)
 
+-- point free solution
+isRotation_2 :: [Char] -> [Char] -> Bool
+isRotation_2 = ap (ap . ((&&) .) . (. length) . (==) . length) (flip isInfixOf . join (++))
