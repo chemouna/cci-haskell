456e8efa77799a6f985c3f2e042747ce7655179f
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-01-30 07:19:46 +0100

    Solution #2 to 2.1

--- src/Chapter1/Q21RemoveDupes.hs
+++ src/Chapter1/Q21RemoveDupes.hs
@@ -1,7 +1,12 @@
-
 module Q21RemoveDupes where
+
+import Data.List
 
 removeDupes :: Eq a => [a] -> [a]
 removeDupes [] = []
 removeDupes (x:xs) = x:filter (/= x) (removeDupes xs)
 
+removeDupes2 :: Eq a => [a] -> [a]
+removeDupes2 = foldl (\seen x -> if x `elem` seen
+                                 then seen
+                                 else seen ++ [x]) []
