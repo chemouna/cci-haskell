30b7e660ce5adaca9d1481ab3f29039f5cf62fb4
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-01-30 07:29:28 +0100

    Solution #2 to 2.2

--- src/Chapter1/Q21RemoveDupes.hs
+++ src/Chapter1/Q21RemoveDupes.hs
@@ -6,7 +6,12 @@ removeDupes :: Eq a => [a] -> [a]
 removeDupes [] = []
 removeDupes (x:xs) = x:filter (/= x) (removeDupes xs)
 
+-- O(n^2) but with Eq constraint its not possible to have an O(n) solution
 removeDupes2 :: Eq a => [a] -> [a]
 removeDupes2 = foldl (\seen x -> if x `elem` seen
                                  then seen
                                  else seen ++ [x]) []
+
+-- an O(n log n) solution but that requires Ord constraint and doesnt preserve the order
+removeDupes3 :: Ord a => [a] -> [a]
+removeDupes3 = map head . group . sort
