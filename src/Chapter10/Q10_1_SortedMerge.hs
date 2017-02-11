e165be4bb73786114f6399ce5eef2bcd4e08543b
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-02-11 11:00:56 +0100

    Adding quickcheck check for sorted merge.

--- src/Chapter10/Q10_1_SortedMerge.hs
+++ src/Chapter10/Q10_1_SortedMerge.hs
@@ -2,8 +2,10 @@
 
 module Q10_1_SortedMerge where
 
+import Data.List
 import Test.QuickCheck
 import Test.QuickCheck.All
+import Test.QuickCheck.Arbitrary
 
 sortedMerge :: Ord a => [a] -> [a] -> [a]
 sortedMerge [] xs = xs
@@ -14,8 +16,14 @@ sortedMerge (x:xs) (y:ys)
 
 -- TODO: how in quicksort to write the invariant that the two lists must be ordered each
 
-prop_sortedMerge_min_is_min_two_list xs ys =
+sorted :: Ord a => [a] -> Bool
+sorted xs = xs == sort xs
+
+prop_sortedMerge_min_is_min_two_list (Ordered xs) (Ordered ys) =
   not (null xs) && not (null ys) ==> minimum (sortedMerge xs ys) == minimum [head xs, head ys]
+
+prop_sortedMerge_equals_size_of_both xs ys = length (sortedMerge xs ys) == length xs + length ys
+
 
 return []
 main = $quickCheckAll
